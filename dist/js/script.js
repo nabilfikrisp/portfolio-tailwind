42978144b1e3f0554ff8e5389eb21192a2d83f77
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-01-05 18:09:49 +0700

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,10 +11,34 @@ hamburger.addEventListener("click", function () {
 window.onscroll = function () {
   const navbar = document.querySelector("#navbar");
   const fixedNav = navbar.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     navbar.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
   } else {
     navbar.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
   }
 };
+
+
+// Click outside hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.toggle('hidden');
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+  } else {
+    html.classList.remove('dark');
+  }
+})
